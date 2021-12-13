5543829111e62de5a66b8f98f80770308f6687f0
lucas mira <lucas-mira1@hotmail.com>
2021-12-13 20:22:49 -0300

    bingo admin password

--- bingoApp/models.py
+++ bingoApp/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class BingoAdmin(models.Model):
     name = models.CharField(max_length=100)
+    password = models.CharField(max_length=50)
 
 
 class AdminNumber(models.Model):
