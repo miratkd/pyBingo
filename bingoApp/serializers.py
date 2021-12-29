5cbc2272c62b4a7adce01b4bf9470aad13e4a29f
lucas mira <lucas-mira1@hotmail.com>
2021-12-29 14:03:13 -0300

    fix card serializer bug

--- bingoApp/serializers.py
+++ bingoApp/serializers.py
@@ -45,7 +45,7 @@ class CardSerializer(serializers.ModelSerializer):
     class Meta:
         model = Card
         fields = ['id', 'name', 'is_bingo', 'admin', 'p1', 'p2', 'p3', 'p4', 'p5', 'p6', 'p7', 'p8', 'p9', 'p10', 'p11',
-                  'p12', 'p13', 'p14', 'p15', 'p16', 'p17', 'p18', 'p19', 'p20', 'p20', 'p22', 'p23', 'p24', 'p25']
+                  'p12', 'p13', 'p14', 'p15', 'p16', 'p17', 'p18', 'p19', 'p20', 'p21', 'p22', 'p23', 'p24', 'p25']
 
     def create(self, validate_data):
         data = validate_data
